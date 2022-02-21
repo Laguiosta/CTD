b50ad418a1e4ee5bf381bdfc5969dea51521ec99
Laguiosta <63820534+Laguiosta@users.noreply.github.com>
2022-02-21 20:57:02 -0300

    Repositó rio DH

---
+++ intro-imp/aulas/aula-14-02/ex2.js
@@ -0,0 +1,10 @@
+let nickname = "Adriano" 
+let age = 19
+let userActive = true 
+
+console.log("Name: ")
+console.log(nickname)
+console.log("Age: ")
+console.log(age)
+console.log("User it's active on sistem? ")
+console.log(userActive)
---
+++ intro-imp/aulas/calculadora/calculadora.js
@@ -0,0 +1,47 @@
+    function somar (a, b) {
+    var somar = a + b;
+    return somar;
+}
+    var somarUm = somar(1, 10)
+
+//
+    function subtracao (a, b) {
+    var subtracao = a - b;
+    return subtracao
+}
+    var subtracaoUm = subtracao(1, 10)
+
+//
+    function multiplicacao (a, b) {
+        var multiplicacao = a * b;
+        return multiplicacao;
+    }
+    var multiplicacaoUm = multiplicacao(2,10)
+
+//
+    function divisao (a,b) {
+        var divisao = a / b
+        return divisao
+    }
+    var divisaoUm = divisao(2,10)
+//
+
+    function  quadradoDoNumero(a) {
+        var quadradoDoNumero =  Math.pow(2, multiplicacao)
+        return quadradoDoNumero
+    }
+    var quadradoDoNumeroUm = quadradoDoNumero(2)
+    console.log(quadradoDoNumeroUm)
+
+
+/*
+    console.log("Teste de Operações / Calculador")
+
+    console.log(somarUm, subtracaoUm);
+    console.log(multiplicacaoUm);
+    console.log(divisaoUm);
+    console.log(divisaoUm);
+*/
+
+
+
