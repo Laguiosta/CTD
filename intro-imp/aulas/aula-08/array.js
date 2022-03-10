38b810ec2c07ba8043828190c9c727f24cf636e6
Laguiosta <63820534+Laguiosta@users.noreply.github.com>
2022-03-10 20:40:12 -0300

    Dale

---
+++ intro-imp/aulas/aula-08/array.js
@@ -0,0 +1,21 @@
+
+let participanteA = [5, 8, 4, 9, 5]
+let participanteB = [8, 7, 8, 6, 8]
+let participanteC = [7, 5, 10, 8, 3] 
+
+console.log(`
+                    Notas:
+            Participante 1  ${participanteA}, 
+            Participante 2  ${participanteB}, 
+            Participante 3  ${participanteC}`
+            );
+
+function pontuaçaoMedia(participanteA, participanteB, participanteC) {
+
+let media = participanteA[0] + participanteA[1] + participanteA[2] + participanteA[3] + participanteA[4]
+            participanteB[0] + participanteB[1] + participanteB[2] + participanteB[3] + participanteB[4]
+            participanteC[0] + participanteC[1] + participanteC[2] + participanteC[3] + participanteC[4]
+
+ return media
+}
+
