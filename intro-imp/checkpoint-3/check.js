5c31d39ff0fa737eb6d5bbc041fd2c104df6a9d0
Laguiosta <63820534+Laguiosta@users.noreply.github.com>
2022-04-17 18:47:35 -0300

    up

--- intro-imp/checkpoint-3/check.js
+++ intro-imp/checkpoint-3/check.js
@@ -1,56 +1,86 @@
 
-//  Passo 1 
-function Alunos(nome, qtdFaltas, notas) {
 
-    this.nome = nome,
-        this.qtdFaltas = qtdFaltas,
-        this.notas = notas,
+
+
+//  Passo 1 
+        function Alunos(nome, qtdFaltas, notas) {
+
+            this.nome = nome,
+            this.qtdFaltas = qtdFaltas,
+            this.notas = notas,
 
 
 // Passo 2
-        calcularMedia = function () {
+        this.calcularMedia = function () {
             let total = 0;
-            for (var i = 0; i < notas.length; i++) {
-                total += notas[i] / notas.length;
-            } return console.log(total)
-            // (Está retortando a média do último aluno criando lá em baixo)
+            for (let i = 0; i < this.notas.length; i++) {
+
+                total = total + this.notas[i];
+
+            }
+            return total / this.notas.length;
 
         }
 
+// Continuação do passo 2
 
-    //(Não sei onde está o erro, mas para funcionar é só apagar esse if)   addFaltas = function(Alunos) {
-    if (qtdFaltas > 1) {
-        qtdFaltas++ + 1
+    this.addQtdFaltas = function () {
+        qtdFaltas = this.qtdFaltas++
+         
+        return console.log(this.qtdFaltas);
+    // Está retornando "Undefined no final, mas a função está funcionando"
     }
+
+   
+
 }
+
 
 // Passo 3
-let curso = {
-    curso: "BackEnd",
-    notaDeAprovacao: 8,
-    maxFaltas: 2,
-    listaAlunos: ["Adriano", "Guilherme", "Giulia"]
-}
+        let curso = {
 
+            curso: "BackEnd",
+            notaDeAprovacao: 8,
+            maxFaltas: 2,
+            listaAlunos: [],
+
+// Passo 4
+                adicionarAluno: function (nome, qtdFaltas, notas) {
+                let aluno = new Alunos (nome, qtdFaltas, notas)
+                this.listaAlunos.push(aluno);
+
+                return console.log(this.listaAlunos)
+
+            },
 // Passo 5
-function aprovacao(aluno) {
-    if (nota > notaDeAprovacao && faltas < maxFaltas) {
-        console.log("Aprovado")
-    }
+
+                aprovacao:function (aluno) {
+                if (aluno.calcularMedia() >= this.notaDeAprovacao && aluno.qtdFaltas < this.maxFaltas) {
+
+                    return console.log("Aprovado")
+
+
+                }
+            }
 
 }
+                
+
 
 
 const aluno1 = new Alunos('Guilherme', 1, [10, 9, 8]);
 const aluno2 = new Alunos('Giulia', 0, [9, 10, 7]);
 const aluno3 = new Alunos('Laguiosta', 0, [10, 10, 2]);
 
-console.log(aluno1);
-console.log(aluno2);
-console.log(aluno3);
-console.log(calcularMedia(aluno1));
+// Teste do passo 2.1
+console.log(aluno1.calcularMedia(10, 9, 8));
+// Teste do passo 2.2
+aluno1.addQtdFaltas();
+// Teste do passo 4
+curso.adicionarAluno("teste1", 2, [10,10,10]);
+curso.adicionarAluno("teste2", 2, [10,10,10]);
+// Teste do passo 5
+curso.aprovacao(aluno2);
 
 
-
-
-
+        
